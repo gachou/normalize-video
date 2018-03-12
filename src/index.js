e609de1815f6d870589824038b2aa62743e07bff
Nils Knappmeier <github@knappi.org>
2018-03-12 20:42:02 +0100

    Initial check-in

---
+++ src/index.js
@@ -0,0 +1,17 @@
+/*!
+ * normalize-video <https://github.com/gachou/normalize-video>
+ *
+ * Copyright (c) 2018 Nils Knappmeier.
+ * Released under the MIT license.
+ */
+
+'use strict'
+
+module.exports = normalizeVideo
+/**
+ * Describe your module here
+ * @public
+ */
+function normalizeVideo () {
+  return "my name is normalizeVideo"
+}
