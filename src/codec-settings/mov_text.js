a02d0140a8c75c3e9b7d08287dc94294e774b6a0
Nils Knappmeier <github@knappi.org>
2018-03-17 15:47:55 +0100

    Adaption to ffprobe 3.4.2 (pgssub -> hdmv_pgs_subtitle)

--- src/codec-settings/mov_text.js
+++ src/codec-settings/mov_text.js
@@ -10,6 +10,7 @@ module.exports = {
 function computeSettings (streamProbe) {
   const result = []
   switch (streamProbe.codec_name) {
+    case 'hdmv_pgs_subtitle':
     case 'pgssub':
       // Subtitles are image based and cannot be converted to mov_text, so we ignore them
       // see https://stackoverflow.com/questions/36326790/cant-change-video-subtitles-codec-using-ffmpeg
